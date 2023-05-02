b74ceb73ec8b4d5b57191917ad22ef2f15aaf9db
Mastertito641 <maruchito2003@gmail.com>
2023-05-02 15:07:46 -0500

    mi primera aplicacion 1

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primera aplicacion</title>
+    <link rel="stylesheet"href="css/bootstrap.min.css"><link>
+    <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Identificador</label>
+    <input type="text" class="form-control" id="identificador" name="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Nombre</label>
+    <input type="text" class="form-control" id="nombre" name="nombre">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="text" class="form-label"> Detalle></label>
+    <label class="form-check-label" for="exampleCheck1"></label>
+  </div>
+  <button type="submit" class="btn btn-primary">registrar</button>
+</form>
+
+    
+</body>
+</html>
